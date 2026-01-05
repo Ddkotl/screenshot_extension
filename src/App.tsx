d43eeb9fdb219d24947378ca46baeb2cc7286f67
Dkotl <126883337+Dkotl@users.noreply.github.com>
2026-01-05 17:33:41 +0000

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
 
-export function App() {
-    return <div className="p-2 mx-auto text-center">hello</div>
+export default function App() {
+    async function handleClick() {
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        console.log(tab)
+        if (tab.id) {
+            chrome.tabs.sendMessage(tab.id, { action: "start-selection" });
+        }
+    }
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <div className="p-2 mx-auto text-center flex flex-col justify-center max-w-md">
+                <div className="flex items-center justify-center gap-2"><ModeToggle />{chrome.i18n.getMessage("extension_name")} </div>
+                <Button onClick={handleClick}>{chrome.i18n.getMessage("make_screenshot")}</Button>
+            </div>
+        </ThemeProvider>
+    );
 }
-
-export default App;
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,24 @@
+import { Moon, Sun } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { useTheme } from "@/components/theme-provider"
+
+export function ModeToggle() {
+    const { setTheme, theme } = useTheme();
+
+    function hanleClick() {
+        if (theme === "light" || theme === "system") {
+            setTheme("dark");
+        }
+        if (theme === "dark") {
+            setTheme("light");
+        }
+    }
+
+    return (
+        <Button variant="ghost" size="icon" onClick={() => hanleClick()}>
+            <Sun className="text-fio h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="text-fio absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </Button>
+    );
+}
